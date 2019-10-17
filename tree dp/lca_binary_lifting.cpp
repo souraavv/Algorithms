7fffb1308ed13fa3098ae8bab04e01cb2310cd66
shrabana99 <49238570+shrabana99@users.noreply.github.com>
2019-10-17 14:42:56 +0530

    Update lca_binary_lifting.cpp

--- tree dp/lca_binary_lifting.cpp
+++ tree dp/lca_binary_lifting.cpp
@@ -1,30 +1,20 @@
+// https://www.spoj.com/problems/LCA/
+
 #include <bits/stdc++.h>
 #define ll long long
-#define maxn 100005
+#define maxn 1005
 #define vec2d vector<vector<ll>>
 using namespace std;
 
 vector<ll> vec[maxn];
 ll tim, tin[maxn], tout[maxn], len;
-vector<vector<ll>> lca; // vector of vector
+ll lca[maxn][51];
 // lca[u][i] is the node that is the 2^i the ancestor of node u
 
 void dfs(ll u, ll p){
     tin[u] = ++tim; // in time
     lca[u][0] = p; //  p is the 2^0 ie the 1st ancestor of node u
     for(ll i=0; i<len; i++){
-        /*
-                    x
-                   / x is 2^ith ancestor of y
-                  y                         \ then x is 2^(i+1) ancestor of u
-                 / y is 2^ith ancestor of u /
-                u
-
-        */
-
-        // x is 2^(i+1) ancestor of u
-        //or 2^(i+1) ancestor of u is the 2^ith ancestor of y
-        // or 2^(i+1) ancestor of u is the 2^ith ancestor of lca[u][i] as y is stored in the lca table
         lca[u][i+1] = lca[lca[u][i]][i];
     }
     for(ll i=0; i<vec[u].size(); i++){
@@ -45,22 +35,13 @@ bool is_ancestor(ll ch, ll par){ // if v is ancestor of u ,ie u is in the subtre
 }
 
 ll LCA(ll u, ll v){
-    if(u==v) return v;
+    if(u == v) return u;
     if(is_ancestor(u, v)){//if v is ancestor of u
         return v;
     }
     else if(is_ancestor(v, u)){ //if u is ancestor of v
         return u;
     }
-    //if u and v are in different subtree
-    // we will fix v
-    // for 2^l th ancestor to 2^0 th ancestor of u , we will check if that is the common ancestor of u, v
-    /*if 2^i th ancestor of u ie (lca[u][i]) is also ancestor of v (satisfies is_ancestor())
-     we just decrement i , as we need the LOWEST OF COMMON ANCESTORs
-     if 2^i th ancestor of u ie (lca[u][i]) is NOT an ancestor of v (!is_ancestor())
-     we will raise the u node to its 2^i th ancestor and continue
-
-    */
     for(ll i=len; i>=0; i--){
         if(!is_ancestor(v, lca[u][i])){
             u = lca[u][i];
@@ -69,26 +50,38 @@ ll LCA(ll u, ll v){
     return lca[u][0];
 }
 int main(){
-    ll n;
-    cin >> n;
-
-    for(ll i=0; i<n-1; i++){
-        ll x, y;
-        cin >> x >> y;
-        vec[x].push_back(y);
-        vec[y].push_back(x);
-    }
-    len = ceil(log2(n)); // log2(n)
-    tim = 0;
-    memset(tin, 0, sizeof(tin));
-    memset(tout, 0, sizeof(tout));
-    lca.assign(n, vector<ll>(len+1));
-    dfs(1, 1);
-    ll q;
-    cin>>q;
-    while(q--){
-        ll x, y;
-        cin >> x >> y;
-        cout << LCA(x, y) << endl;
+    //ios_base::sync_with_stdio(false);cin.tie(0);
+    ll t; cin>>t;
+    for(ll tc = 0;tc<t; tc++){
+        ll n;
+        cin >> n;
+        // clear
+        len = ceil(log2(n)); // log2(n)
+        memset(lca, 0, sizeof(lca));
+        for(ll i=0; i<=n; i++){
+            vec[i].clear();
+        }
+        tim = 0;
+        memset(tin, 0, sizeof(tin));
+        memset(tout, 0, sizeof(tout));
+        for(ll i=1; i<=n; i++){
+            ll m;
+            cin >> m;
+            while(m--){
+            ll x; cin >> x;
+            vec[x].push_back(i);
+            vec[i].push_back(x);
+            }
+        }
+        dfs(1, 1);
+        ll q;
+        cin>>q;
+        cout << "Case " << tc+1 << ":\n";
+        while(q--){
+            ll x, y;
+            cin >> x >> y;
+            cout << LCA(x, y) << endl;
+        }
     }
 }
+
